814fcfb19a237ea9b2d6241b4cb807e5a1c40009
apinanyogaratnam <apinanapinan@icloud.com>
2022-06-26 20:47:20 -0400

    chore: adding nvm to path in linux script

--- linux.sh
+++ linux.sh
@@ -13,3 +13,7 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install zsh
 brew install zsh rust kubernetes-cli kubectx awscli gh minikube node nvm pnpm postgresql python@3.10 sqlite terraform
+
+# add nvm to path
+mkdir ~/.nvm
+export NVM_DIR="$HOME/.nvm"
